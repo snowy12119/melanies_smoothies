61635d6260fb19dee76356239371516951ca0560
snowy12119 <m.snowden@nhs.net>
2026-07-10 17:06:57 +0100

    Fix session initialization and clean up code

--- orders.py
+++ orders.py
@@ -13,12 +13,11 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your smoothie will be: ')
 
+
 cnx = st.connection ("snowflake")
 session = cnx.session()
 
 
-
-session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 # st.dataframe(data=my_dataframe, use_container_width=True);
 
