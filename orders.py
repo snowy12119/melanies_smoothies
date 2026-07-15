990e24e67c70a52cf59fd7fc43fd6fc1871eb62b
snowy12119 <m.snowden@nhs.net>
2026-07-15 08:59:53 +0100

    Replace text display with dataframe for smoothie fruits
    Comment out the previous text display and add a dataframe display for the smoothie fruit response.

--- orders.py
+++ orders.py
@@ -5,8 +5,8 @@ from snowflake.snowpark.functions import col
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
-
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 # Write directly to the app
