cbb793da0b6817f271585fcbde9a5d3b748abbda
snowy12119 <m.snowden@nhs.net>
2026-07-15 08:58:03 +0100

    Parse JSON response from Smoothiefroot API

--- orders.py
+++ orders.py
@@ -5,7 +5,7 @@ from snowflake.snowpark.functions import col
 
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
 
