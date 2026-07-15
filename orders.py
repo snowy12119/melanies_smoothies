32595578377fd63e5bd6f6f3c1615cd08fded661
snowy12119 <m.snowden@nhs.net>
2026-07-15 08:56:49 +0100

    Fix URL formatting in API request

--- orders.py
+++ orders.py
@@ -4,7 +4,7 @@ import requests
 from snowflake.snowpark.functions import col
 
 
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
 
 
