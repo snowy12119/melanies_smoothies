a6a638c692918a1f8be987547d517dec27aecd43
snowy12119 <m.snowden@nhs.net>
2026-07-15 09:00:44 +0100

    Restore smoothie fruit API call and dataframe display
    Reintroduce smoothie fruit API call and display data in dataframe.

--- orders.py
+++ orders.py
@@ -4,9 +4,7 @@ import requests
 from snowflake.snowpark.functions import col
 
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 
 # Write directly to the app
@@ -54,3 +52,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
