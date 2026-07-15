cc5ddacc667581d41d533f2215fb0025fcf965a2
snowy12119 <m.snowden@nhs.net>
2026-07-15 10:38:08 +0100

    Modify fruit options selection to include 'SEARCH_ON'
    Added 'SEARCH_ON' column to the fruit options selection and updated dataframe display.

--- orders.py
+++ orders.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 
@@ -23,9 +24,9 @@ cnx = st.connection ("snowflake")
 session = cnx.session()
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
-# st.dataframe(data=my_dataframe, use_container_width=True);
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True);
+st.stop()
 
 ingredients_list = st.multiselect(
 'Choose up to 5 ingredients:'
