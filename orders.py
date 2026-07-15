0f898bfd9a5bd34479008758ee548019a01e1497
snowy12119 <m.snowden@nhs.net>
2026-07-15 10:50:31 +0100

    Fix variable name in API request for fruit data

--- orders.py
+++ orders.py
@@ -49,7 +49,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{SEARCH_ON}")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}")
         #st.text(smoothiefroot_response.json())
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         # st.write(ingredients_string)
